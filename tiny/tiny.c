5c796a1042061bb0e0414ff1576ebaf247153ad6
LemonEaterMJ <98jjyoung@gmail.com>
2022-12-12 19:49:30 +0900

    tiny server success

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -63,7 +63,10 @@ void doit(int fd) {
 	read_requesthdrs(&rio);			// reads and ignores request hdrs.
 
 
-	/* Parse URI from GET request */
+	/* Parse URI from GET request 
+	 * filename will filled with xxx.html for appropriate request 
+	 * gets arguments for cgi and fills it to cgiargs
+	 */
 	is_static = parse_uri(uri, filename, cgiargs);
 
 	/* fail check : does file exist? */
@@ -94,6 +97,230 @@ void doit(int fd) {
 	}
 
 }
+
+
+
+/*
+ *  FUNCTION : clienterror 
+ * 	called when other functions check error 
+ *  used for displaying ERROR
+ * 	1. build HTTP response : with sprintf (html 형식)
+ *  2. print HTTP response : with rio_writen
+ * 
+ */
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
+                    char *longmsg) {
+	char buf[MAXLINE], body[MAXBUF];
+
+	/* Build HTTP response body */
+	sprintf(body, "<html><title>Tiny Error</title>");
+	sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+	sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+	sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+	sprintf(body, "%s<hr><em>The Tiny Web Server</em>\r\n", body);
+
+	/* print HTTP Response */
+	sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-type: text/html\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+	Rio_writen(fd, buf, strlen(buf));
+	Rio_writen(fd, body, strlen(body));
+}
+
+
+
+/*
+ * FUNCTION : read_requesthdrs 
+ * in this version, tiny reads but ignores headers 
+ * empty text line will terminate request headers 
+ * 		check empty text line with \r\n
+ */
+void read_requesthdrs(rio_t *rp) {
+	char buf[MAXLINE];
+
+	Rio_readlineb(rp, buf, MAXLINE);	//always read first request line
+
+	/* check for emtpy line(ternimation) \r\n 
+		if not empty, just print 
+	*/ 
+	while(strcmp(buf, "\r\n")) {		
+		Rio_readlineb(rp, buf, MAXLINE);
+		printf("%s", buf);
+	}
+	return ;
+}
+
+
+
+
+
+
+/* 
+ * FUNCTION : parse_uri
+ * parse uri string for cgi-bin -> RETURN flag for static(1)/dynamic(0)
+ * 		if cgi-bin : dynamic content
+ * 
+ * 1. parse URI into filename and CGI argument string 
+ * depending on static/dynamic, 
+ * 2. if static, clear argument string 
+ * 3. convert URI into pathname xxxx.html
+ * 4. if end with / : call for home, URI home.html
+ * 5. if dynamic, pointer cgiargs points to args 
+ * 6. convert remaining URI inro pathname xxxx.html
+ */
+int parse_uri(char *uri, char *filename, char *cgiargs) {
+	char *ptr;
+
+	// 1. find cgi-bin
+	if (!strstr(uri, "cgi-bin")) {	// STATIC 
+		strcpy(cgiargs, "");		// clear arguments
+		strcpy(filename, ".");	
+		strcat(filename, uri);		// convert URI to pathname 
+		if (uri[strlen(uri) - 1] == '/') {	// if ends with /
+			// convert URI to default home.html
+			strcat(filename, "home.html");
+		}
+		return 1;
+	} else {						// DYNAMIC
+		// search for arg separator ?
+		ptr = index(uri, '?');
+		if (ptr) {
+			strcpy(cgiargs, ptr + 1);
+			*ptr = '\0';
+		} else {	// no ? separator 
+			strcpy(cgiargs, "");
+		}
+
+		// convert URI to pathname 
+		strcpy(filename, ".");
+		strcat(filename, uri);
+		return 0;
+	}
+
+	return 0;
+}
+
+
+
+/*
+ * FUNCTION : serve_static 
+ * called from doit() 
+ * serves static content 
+ * 1. parse filename and filetype with get_filetype()
+ * 2. send response headers to client(end with empty line)
+ * 3. open 'filename' and get descriptor 
+ * 4. malloc file to virtual memory
+ * 5. close file(since malloced, not needed)
+ * 6. write to fd(the one we send to client)
+ * 7. free malloced source file pointer
+ * 
+ */
+void serve_static(int fd, char *filename, int filesize) {
+	int srcfd;		// source file descriptor 
+	// source pointer
+	char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+	/* parse filename and filetype */
+	get_filetype(filename, filetype);
+
+	/* prepare response hdrs in buffer */
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+	sprintf(buf, "%sConnection: close\r\n", buf);
+	sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+	sprintf(buf, "%sContent-Type: %s\r\n\r\n", buf, filetype);
+
+	// write buf to fd (file descriptor)
+	Rio_writen(fd, buf, strlen(buf));
+
+	/* print response hdrs */
+	printf("Response headers: \n");
+	printf("%s", buf);
+
+	/* Send response body to client */
+	// open 'filename' and get descriptor source file descriptor
+	srcfd = Open(filename, O_RDONLY, 0);
+
+	/* malloc file to virtual memory(source ptr) */
+	srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+	Close(srcfd);						// Close file 
+	Rio_writen(fd, srcp, filesize);		// write srcp to fd
+	Munmap(srcp, filesize);				// free
+
+}
+
+
+
+
+/* 
+ * FUNCTION : get_filetype()
+ * parse filename and filetype 
+ * this version of tiny supports 5 filetypes : 
+ * 		html : text/html	
+ * 		image : image/png, image/jpeg
+ * 		gif : image/gif
+ * 		text : text/plain
+ */
+void get_filetype(char *filename, char *filetype) {
+	if (strstr(filename, ".html")) {
+		strcpy(filetype, "text/html");
+	} else if (strstr(filename, ".gif")) {
+		strcpy(filetype, "image/gif");
+	} else if (strstr(filename, ".png")) {
+		strcpy(filetype, "image/png");
+	} else if (strstr(filename, ".jpg")) {
+		strcpy(filetype, "image/jpeg");
+	} else {	// plain text
+		strcpy(filetype, "text/plain");
+	}
+}
+
+
+
+
+/* 
+ * FUNCTION : serve_dynamic
+ * called from doit()
+ * serves dynamic content by forking child process
+ * 
+ * 1. Send success response line & server header to client  
+ * 		other HTTP responses will be sent by cgi
+ * 		notice there is no print process at this stage
+ * 2. fork a new child process
+ * 3. initialize QUERY_STRING with cgi arg (from request uri)
+ * 4. redirect child stdout to connected file descriptor 
+ * 5. load & run cgi program(uses QUERY_STRING)
+ * 		child process outputs go directly to client 
+ * 6. parent waiting for child to finish(and then kills it)
+ */
+void serve_dynamic(int fd, char *filename, char *cgiargs) {
+	// connected file descriptor fd
+	char buf[MAXLINE], *emptylist[] = {NULL};
+
+	/* Prepare first part of HTTP response */
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	Rio_writen(fd, buf, strlen(buf));	// write buf to fd 
+	sprintf(buf, "Server: Tiny Web Server\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+
+	/* fork new child process */
+	// if successful, return 0
+	if (Fork() == 0) {
+		/* Real server would set all CGI vars here*/
+		setenv("QUERY_STRING", cgiargs, 1);		
+
+		/* Redirect stdout to client*/
+		Dup2(fd, STDOUT_FILENO);
+
+		/* Run cgi program */
+		Execve(filename, emptylist, environ);
+	}
+	Wait(NULL);		
+	//parent waiting for child to finish(and then kills it)
+}
+
 
 
 
